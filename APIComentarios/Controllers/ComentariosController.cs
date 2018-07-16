e7633a93d88ea46d57a9e3e247547b6a51d7f1f1
Renato Groffe <renato.groffe@yahoo.com.br>
2018-07-16 00:30:46 -0300

    ASP.NET Core 2.1 + ApiController
    ASP.NET Core 2.1 + ApiController

---
+++ APIComentarios/Controllers/ComentariosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using APIComentarios.Data;
+using APIComentarios.Models;
+
+namespace APIComentarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Comentario> Get(
+            [FromServices]ApplicationDbContext context)
+        {
+            return context.Comentarios.ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Post(
+            Comentario comentario,
+            [FromServices]ApplicationDbContext context)
+        {
+            comentario.Id = Guid.NewGuid();
+            comentario.Data = DateTime.Now;
+            context.Comentarios.Add(comentario);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ APIComentarios/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using APIComentarios.Data;
+
+namespace APIComentarios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configurando o acesso a dados
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("InMemoryDatabase"));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
